b6b029bb9521f38303835d2e33f4bf88611d55f3
eco747 <>
2026-05-27 10:38:07 +0200

    release: 2.2.12

--- src/components/video/video.ts
+++ src/components/video/video.ts
@@ -14,7 +14,7 @@
  * that can be found in the LICENSE file or at https://opensource.org/licenses/MIT.
  **/
 
-import { Component, ComponentProps } from '../../core/component';
+import { Box, Component, ComponentProps } from 'x4js';
 
 import "./video.module.scss"
 
@@ -25,47 +25,106 @@ interface VideoProps extends ComponentProps {
 	stream: MediaStream;
 }
 
-import { class_ns } from '../../core/core_tools';
-export class Video extends Component {
+/**
+ * 
+ */
+
+export class Video extends Box {
+	
+	private _vplayer: HTMLVideoElement;
+
 	constructor( props: VideoProps ) {
-		super( { tag: 'video', ...props } );
+		super( props );
+
+		const el = new Component( { cls: "vplayer", tag: 'video', } );
+		this.setContent( el );
+
+		this._vplayer = el.dom as HTMLVideoElement;
 
 		if( props.autoplay !== undefined ) {
-			this.AutoPlay = props.autoplay;
+			this.autoPlay = props.autoplay;
 		}
 
 		if( props.muted !== undefined ) {
-			this.Muted = props.muted;
+			this.mute( props.muted );
 		}
 
 		if( props.playsInline !== undefined ) {
-			this.PlaysInline = props.playsInline;
+			this.playsInline = props.playsInline;
 		}
 
-		this.Stream = props.stream;
+		this.stream = props.stream;
 	}
 
-	set AutoPlay( set: boolean ) {
-		(this.dom as HTMLVideoElement).autoplay = set;
+	get player( ) {
+		return this._vplayer;
 	}
 
-	set Muted( set: boolean ) {
-		(this.dom as HTMLVideoElement).muted = set;
-	}
-	
-	set PlaysInline( set: boolean ) {
-		(this.dom as HTMLVideoElement).playsInline = set;
+	get volume( ) {
+		return this.player.volume;
 	}
 
-	set Stream( stream: MediaStream ) {
-		(this.dom as HTMLVideoElement).srcObject = stream;
+	set volume( vol: number ) {
+		this.player.volume = vol;
+	}
+
+	get autoPlay( ) {
+		return this.player.autoplay;
+	}
+
+	set autoPlay( set: boolean ) {
+		this.player.autoplay = set;
+	}
+
+	get playsInline( ) {
+		return this.player.playsInline;
+	}
+
+	set playsInline( set: boolean ) {
+		this.player.playsInline = set;
+	}
+
+	set stream( stream: MediaStream ) {
+		this.player.srcObject = stream;
 	}
 
 	get videoWidth( ) {
-		return (this.dom as HTMLVideoElement).videoWidth;
+		return this.player.videoWidth;
 	}
 
 	get videoHeight( ) {
-		return (this.dom as HTMLVideoElement).videoHeight;
+		return this.player.videoHeight;
+	}
+
+	get loop( ) {
+		return this.player.loop;
+	}
+
+	set loop( set: boolean ) {
+		this.player.loop = set;
+	}
+
+	get duration( ) {
+		return this.player.duration;
+	}
+
+	get muted( ) {
+		return this.player.muted;
+	}
+
+	mute( set: boolean ) {
+		this.player.muted = set;
+	}
+		
+	get paused( ) {
+		return this.player.paused;
+	}
+
+	pause( ) {
+		this.player.pause( );
+	}
+
+	play( ) {
+		this.player.play( );
 	}
 }
