b502448a77a3260771b59bbccbda1a41807d1f41
eco747 <>
2026-09-22 14:03:02 +0200

    undefined use

--- src/core/core_element.ts
+++ src/core/core_element.ts
@@ -259,8 +259,10 @@ export class CoreElement<E extends EventMap = EventMap> {
 
 	cleanUp( ) {
 		this.clearTimeouts( );
-		this.#cleanup?.forEach( x => x() );
-		this.#cleanup.length = 0;
+		if( this.#cleanup ) {
+			this.#cleanup.forEach( x => x() );
+			this.#cleanup.length = 0;
+		}
 	}
 
 	// :: EVENTS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
